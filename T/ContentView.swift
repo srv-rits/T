2f1ab5bea3d2e6fcdaf604bdfa6c1917534c2b37
Sourav Mishra <sourav@swnglobal.com>
2023-09-11 17:50:06 +0530

    Initial Commit

---
+++ T/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  T
+//
+//  Created by Sourav Mishra on 11/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ T/TApp.swift
@@ -0,0 +1,17 @@
+//
+//  TApp.swift
+//  T
+//
+//  Created by Sourav Mishra on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
